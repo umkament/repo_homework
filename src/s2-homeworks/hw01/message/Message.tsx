a86e8e6c84f8cfeeed99640d826f9640441321f9
umkament <97944243+umkament@users.noreply.github.com>
2023-03-06 11:13:34 +0300

    hw1.2

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -13,25 +13,25 @@ const Message = (props: MessagePropsType) => {
      <div id={'hw1-message-' + props.message.message} className={s.message}>
        <div className={s.imageAndText}>
          <img
-            id={'hw1-avatar-' + props.message.user.avatar}
+            id={'hw1-avatar-1' + props.message.user.avatar}
             // создаёт студент
             src={props.message.user.avatar} alt={'avatar'}
             //
          />
          <div className={s.text}>
-           <div id={'hw1-name-' + props.message.user.name} className={s.name}>
+           <div id={'hw1-name-1' + props.message.user.name} className={s.name}>
              {/*создаёт студент*/}
              {props.message.user.name}
              {/**/}
            </div>
-           <pre id={'hw1-text-' + props.message.message.text} className={s.messageText}>
+           <pre id={'hw1-text-1' + props.message.message.text} className={s.messageText}>
                         {/*создаёт студент*/}
              {props.message.message.text}
              {/**/}
                     </pre>
          </div>
        </div>
-       <div id={'hw1-time-' + props.message.message.time} className={s.time}>
+       <div id={'hw1-time-1' + props.message.message.time} className={s.time}>
          {/*создаёт студент*/}
          {props.message.message.time}
          {/**/}
