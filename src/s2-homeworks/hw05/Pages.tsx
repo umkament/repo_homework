3bd8a3cc015cf88d0ddc5ddb94b3d17f9e78139e
umkament <97944243+umkament@users.noreply.github.com>
2023-03-09 22:21:41 +0300

    hw5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -26,7 +26,7 @@ function Pages() {
                 <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route element={<Error404 />}/>
+                <Route path={'/*'} element={<Error404 />}/>
             </Routes>
         </div>
     )
