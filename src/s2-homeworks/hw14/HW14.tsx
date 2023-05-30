39e6fc2901fa8ae4c781429d639b8dda054d4d61
umkament <97944243+umkament@users.noreply.github.com>
2023-05-30 20:03:42 +0300

    hw14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -16,7 +16,7 @@ import {useSearchParams} from 'react-router-dom'
 const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
-            'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
+'https://samurai.it-incubator.io/api/3.0/homework/test2',
             {params: {find}}
         )
         .catch((e) => {
@@ -35,7 +35,8 @@ const HW14 = () => {
         getTechs(value)
             .then((res) => {
                 // делает студент
-
+                setLoading(false)
+if (res) setTechs(res.data.techs)
                 // сохранить пришедшие данные
 
                 //
@@ -45,10 +46,10 @@ const HW14 = () => {
     const onChangeText = (value: string) => {
         setFind(value)
         // делает студент
-
+setLoading(true)
         // добавить/заменить значение в квери урла
         // setSearchParams(
-
+setSearchParams({find: value})
         //
     }
 
